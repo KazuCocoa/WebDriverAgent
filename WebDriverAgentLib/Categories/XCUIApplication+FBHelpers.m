c368f3106e332bc82a811d84923658b86854342c
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-03-20 00:18:28 +0900

    tweak commands for TV (#4)

--- WebDriverAgentLib/Categories/XCUIApplication+FBHelpers.m
+++ WebDriverAgentLib/Categories/XCUIApplication+FBHelpers.m
@@ -168,4 +168,13 @@ const static NSTimeInterval FBMinimumAppSwitchWait = 3.0;
           fb_firstMatch];
 }
 
+#if TARGET_OS_TV
+- (XCUIElement *)fb_focusedElement
+{
+  return [[[self descendantsMatchingType:XCUIElementTypeAny]
+           matchingPredicate:[NSPredicate predicateWithFormat:@"hasFocus == true"]]
+          fb_firstMatch];
+}
+#endif
+
 @end
