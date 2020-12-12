7bccc1a5a0628c0a671cda3757f3b8fa50060dd7
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-12-12 14:55:17 -0800

    tmp as workaround

--- Scripts/carthage-wrapper.sh
+++ Scripts/carthage-wrapper.sh
@@ -4,6 +4,7 @@ set -e
 
 clang_version=$(clang --version | python3 -c "import sys, re; print(re.findall(r'clang-([0-9.]+)', sys.stdin.read())[0])")
 CLANG_XCODE12_BETA3="1200.0.26.2"
+CLANG_XCODE12_3_RC="1200.0.32.28"
 CLANG_XCODE13="1300.0.0.0"
 need_workaround=$(python3 -c "vtuple = lambda ver: tuple(map(int, ver.split('.'))); print(int(vtuple('$CLANG_XCODE12_BETA3') <= vtuple('$clang_version') < vtuple('$CLANG_XCODE13')))")
 
@@ -21,6 +22,17 @@ trap 'rm -f "$xcconfig"' INT TERM HUP EXIT
 # the build will fail on lipo due to duplicate architectures.
 echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64__XCODE_1200 = arm64 arm64e armv7 armv7s armv6 armv8' >> $xcconfig
 echo 'EXCLUDED_ARCHS = $(inherited) $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_$(EFFECTIVE_PLATFORM_SUFFIX)__NATIVE_ARCH_64_BIT_$(NATIVE_ARCH_64_BIT)__XCODE_$(XCODE_VERSION_MAJOR))' >> $xcconfig
-echo 'SUPPORTED_PLATFORMS = iphonesimulator' >> $xcconfig
 
-XCODE_XCCONFIG_FILE="$xcconfig" carthage "$@"
+need_workaround_supported_version=$(python3 -c "vtuple = lambda ver: tuple(map(int, ver.split('.'))); print(int(vtuple('$CLANG_XCODE12_3_RC') <= vtuple('$clang_version') < vtuple('$CLANG_XCODE13')))")
+if ! [[ $need_workaround_supported_version -ne 1 ]]; then
+  # For Xcode 12.3+
+  echo 'SUPPORTED_PLATFORMS = iphonesimulator' >> $xcconfig
+fi
+
+#XCODE_XCCONFIG_FILE="$xcconfig" carthage "$@"
+
+# carthage checkout
+# carthage build # with each SUPPORTED_PLATFORMS and each project. Should specify scheme...
+
+ /usr/bin/xcrun xcodebuild -project /Users/kazuaki/GitHub/WebDriverAgent/Carthage/Checkouts/CocoaAsyncSocket/CocoaAsyncSocket.xcodeproj -scheme iOS\ Framework -configuration Release -derivedDataPath /Users/kazuaki/Library/Caches/org.carthage.CarthageKit/DerivedData/12.3_12C33/CocoaAsyncSocket/72e0fa9e62d56e5bbb3f67e9cfd5aa85841735bc -sdk iphonesimulator -destination platform=iOS\ Simulator,id=51CDCB06-1EB6-40FF-A450-CE378127874A -destination-timeout 3 ONLY_ACTIVE_ARCH=NO CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY= CARTHAGE=YES build
+ /usr/bin/xcrun xcodebuild -project /Users/kazuaki/GitHub/WebDriverAgent/Carthage/Checkouts/YYCache/Framework/YYCache.xcodeproj -scheme YYCache\ iOS -configuration Release -derivedDataPath /Users/kazuaki/Library/Caches/org.carthage.CarthageKit/DerivedData/12.3_12C33/YYCache/1.1.2 -sdk iphonesimulator -destination platform=iOS\ Simulator,id=51CDCB06-1EB6-40FF-A450-CE378127874A -destination-timeout 3 ONLY_ACTIVE_ARCH=NO CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY= CARTHAGE=YES build
